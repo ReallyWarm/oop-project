3acd60e82c3f5f8eb18a4d86b1e503d140e0a2d4
BBamkorphai <111273264+BBamkorphai@users.noreply.github.com>
2023-03-15 16:29:26 +0700

    Explain attribute

--- web_app/class_object/order.py
+++ web_app/class_object/order.py
@@ -3,7 +3,10 @@ import shoppingcart
 class Order:
     def __init__(self, orders, price, shipping, status, delivery_time) -> None:
 # order is list of the tool that customer bought combine with the price of that tool
-# price 
+# price of shipping and total
+# shipping shipping by who
+# status status of shipping stage , update to the date somehow
+# delivery_time, rely on the shipping company.
           self._orders = orders
           self._price = price
           self._shipping = shipping
