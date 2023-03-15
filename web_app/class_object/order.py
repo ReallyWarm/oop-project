34b1cd9d84b139fb22d95913b2558ce1b84a1086
pithakpong <111265617+pithakpong@users.noreply.github.com>
2023-03-15 20:41:27 +0700

    update attribute

---
+++ web_app/class_object/order.py
@@ -0,0 +1,8 @@
+class Order : 
+    def __init__(self,orders,price,shipping,status,delivery_time):
+        self.orders = orders
+        self.price = price
+        self.shipping = shipping
+        self.status = status
+        self.delivery_time = delivery_time
+        
