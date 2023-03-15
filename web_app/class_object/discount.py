6e53127685b010705101ce5f2e439bee371fd4e8
Walan Kitjarak <warmkitjarak@gmail.com>
2023-03-15 14:48:49 +0700

    Add class Wholesale

--- web_app/class_object/discount.py
+++ web_app/class_object/discount.py
@@ -2,3 +2,8 @@ class Coupon:
     def __init__(self, code, discount_value) -> None:
         self._code = code
         self._discount_value = discount_value
+
+class Wholesale:
+    def __init__(self, amount, discount_value) -> None:
+        self._amount = amount
+        self._discount_value = discount_value
