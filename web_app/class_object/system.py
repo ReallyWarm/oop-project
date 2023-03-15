ba2a08d61584d846e77488bf4ba3758c278e5a1c
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-03-15 16:09:39 +0700

    commit

--- web_app/class_object/system.py
+++ web_app/class_object/system.py
@@ -1,5 +1,5 @@
 class System():
-    
+    #Data of coupon and wholesale
     def __init__(self,server_coupon,wholesale):
         self._server_coupon = server_coupon
         self._wholesale = wholesale
