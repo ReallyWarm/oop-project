831d72c364a224802d3844efaf077a2d24214486
Walan Kitjarak <warmkitjarak@gmail.com>
2023-03-15 14:42:40 +0700

    Delete unused file

--- web_app/class_object/shipping.py
+++
@@ -1,5 +0,0 @@
-class Shipping:
-    def __init__(self, status, shipping_price, delivery_time) -> None:
-        self._status = status
-        self._shipping_price = shipping_price
-        self._delivery_time =  delivery_time
--- web_app/class_object/wholesale.py
+++
@@ -1,4 +0,0 @@
-class Wholesale:
-    def __init__(self, amount, discount_value) -> None:
-        self._amount = amount
-        self._discount_value = discount_value
