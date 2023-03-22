73a7a101d3b46fb89ad9fb78358e237803442dbd
Walan Kitjarak <warmkitjarak@gmail.com>
2023-03-22 23:47:26 +0700

    Update CustomerInfo init attributes

--- web_app/class_object/customerinfo.py
+++ web_app/class_object/customerinfo.py
@@ -1,15 +1,19 @@
+from tool import Item
+from wishlish import Wishlist
+from shoppingcart import ShoppingCart
+
 class CustomerInfo:
-    def __init__(self, first_name, last_name, email, company_name,address,my_wishlist,my_shoppingcart,my_order,my_review,used_coupon) -> None:
+    def __init__(self, first_name, last_name, email, company_name) -> None:
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
         self._company_name = company_name
         self._addresses = [ ]
-        self.my_wishlist = my_wishlist
-        self.my_shoppingcart = my_shoppingcart
-        self.my_order = my_order
-        self.my_review = my_review
-        self.used_coupon = used_coupon
+        self.my_wishlist = Wishlist()
+        self.my_shoppingcart = ShoppingCart()
+        self.my_order = [ ]
+        self.my_review = [ ]
+        self.used_coupon = [ ]
 
     def check_coupon(self,coupon):
         pass
