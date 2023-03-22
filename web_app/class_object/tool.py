4e72c18c22e8a14ae2682c5c010a4ca0f20ce572
Walan Kitjarak <warmkitjarak@gmail.com>
2023-03-22 23:53:03 +0700

    Add Tool and Item property

--- web_app/class_object/tool.py
+++ web_app/class_object/tool.py
@@ -1,5 +1,5 @@
 class Tool:
-    def __init__(self, code, name, description, brand, /
+    def __init__(self, code, name, description, brand, \
                  amount, image, price, type_of_tool) -> None:
           self._code = code
           self._name = name
@@ -12,24 +12,32 @@ class Tool:
           self._image = [image]
           self._wholesale = [ ]
 
-    def update_tool(self):
-        pass
+    @property
+    def price(self):
+        return self._price
+    
+    def get_attributes(self, attr):
+        return self.__dict__[attr]
 
     def modify_tool(self):
-        pass
-
-    def create_item(self):
         pass
 
     def add_review(self):
         pass
 
+    def __str__(self) -> str:
+        return str(self.__class__)+'\n'+', '.join(f'{key} : {value}' for key, value in self.__dict__.items())
+
 class Item:
-    def __init__(self, tool, buy_amount, price) -> None: 
+    def __init__(self, tool, buy_amount) -> None: 
         self._tool = tool
         self._buy_amount = buy_amount
-        self._items_price = price * buy_amount
+        self._items_price = tool.price * buy_amount
 
+    @property
+    def tool(self):
+        return self._tool
+    
     def set_amount(self):
         pass
 
