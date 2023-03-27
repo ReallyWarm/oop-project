345440ff854e20316448cd9b37fb6156e7adda65
ReallyWarm <warmkitjarak@gmail.com>
2023-03-28 02:27:18 +0700

    Add search by category name method

--- web_app/class_object/category.py
+++ web_app/class_object/category.py
@@ -9,10 +9,29 @@ class Category:
     def add_type_of_tool(self, type_of_tool):
         self._types_of_tool.append(type_of_tool)
 
+    def search_by_category(self, category_name):
+        searched = { }
+        category_name_lower = category_name.lower()
+        for type in self.types_of_tool:
+            if category_name_lower in type.typename.lower():
+                searched[type.typename] = type
+            
+            for subtype in type.subtypes_of_tool:
+                if category_name_lower in subtype.subtypename.lower():
+                    searched[subtype.subtypename] = subtype
+
+        # searched = [category for  ]
+
+        return searched
+
 class TypeOfTool:
     def __init__(self, name) -> None:
         self._typename = name
         self._subtypes_of_tool = [ ]
+
+    @property
+    def typename(self):
+        return self._typename
 
     @property
     def subtypes_of_tool(self):
@@ -25,6 +44,10 @@ class SubtypeOfTool:
     def __init__(self, name) -> None:
         self._subtypename = name
         self._tools_list = []
+
+    @property
+    def subtypename(self):
+        return self._subtypename
 
     @property
     def tools_list(self):
