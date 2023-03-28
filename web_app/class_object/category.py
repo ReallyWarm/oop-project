32e429bf958b8fbb2627f664dfb6e7f717cfce68
ReallyWarm <warmkitjarak@gmail.com>
2023-03-29 02:04:22 +0700

    New Category class attributes for search

--- web_app/class_object/category.py
+++ web_app/class_object/category.py
@@ -1,4 +1,8 @@
 class Category:
+    _all_types = [ ]
+    _all_subtypes = [ ]
+    _all_tools = [ ]
+
     def __init__(self) -> None:
         self._types_of_tool = [ ]
 
@@ -12,22 +16,30 @@ class Category:
     def search_by_category(self, category_name):
         searched = { }
         category_name_lower = category_name.lower()
-        for type in self.types_of_tool:
+        for type in self._all_types:
             if category_name_lower in type.typename.lower():
                 searched[type.typename] = type
             
-            for subtype in type.subtypes_of_tool:
-                if category_name_lower in subtype.subtypename.lower():
-                    searched[subtype.subtypename] = subtype
-
-        # searched = [category for  ]
+        for subtype in self._all_subtypes:
+            if category_name_lower in subtype.subtypename.lower():
+                searched[subtype.subtypename] = subtype
 
         return searched
 
-class TypeOfTool:
+    def search_by_name(self, name):
+        searched = { }
+        name_lower = name.lower()
+        for tool in self._all_tools:
+            if name_lower in tool.name.lower():
+                searched[tool.name] = tool
+        
+        return searched
+
+class TypeOfTool(Category):
     def __init__(self, name) -> None:
         self._typename = name
         self._subtypes_of_tool = [ ]
+        super()._all_types.append(self)
 
     @property
     def typename(self):
@@ -40,10 +52,11 @@ class TypeOfTool:
     def add_subtype(self, subtype_of_tool):
         self._subtypes_of_tool.append(subtype_of_tool)
 
-class SubtypeOfTool:
+class SubtypeOfTool(Category):
     def __init__(self, name) -> None:
         self._subtypename = name
         self._tools_list = []
+        super()._all_subtypes.append(self)
 
     @property
     def subtypename(self):
@@ -55,3 +68,4 @@ class SubtypeOfTool:
 
     def add_tool(self, tool):
         self._tools_list.append(tool)
+        self._all_tools.append(tool)
