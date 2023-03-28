71b0b1af5d5b2dae72c677e9fc95e24228bc8967
ReallyWarm <warmkitjarak@gmail.com>
2023-03-29 02:05:34 +0700

    Test new search by category and name

--- web_app/home/class_test/search.py
+++ web_app/home/class_test/search.py
@@ -34,9 +34,29 @@ if __name__ == '__main__':
     # Create tools
     drill_1 = Tool('01x', 'Testing drill', 'POWER', 'idk', 10, None, 1000.00, 'Drills')
     drill_2 = Tool('02x', 'faifa drill', 'POWER', 'idk', 12, None, 800.00, 'Drills')
+    saw_1 = Tool('01x', 'Testing saw', 'da-da', 'idk', 10, None, 700.00, 'Saws')
+    saw_2 = Tool('02x', 'see saw', 'da-da', 'idk', 12, None, 1000.00, 'Saws')
+    hammer_1 = Tool('01x', 'Testing hammer', 'bang bang', 'idk', 10, None, 200.00, 'Hammers')
+    hammer_2 = Tool('02x', 'toob hammer', 'bang bang', 'idk', 12, None, 100.00, 'Hammers')
+    watering_1 = Tool('01x', 'Testing watering can', 'WET', 'idk', 10, None, 50.00, 'Waterings')
+    watering_2 = Tool('02x', 'Shower watering', 'WET', 'idk', 12, None, 100.00, 'Waterings')
     drills_type.add_tool(drill_1)
     drills_type.add_tool(drill_2)
+    saws_type.add_tool(saw_1)
+    saws_type.add_tool(saw_2)
+    hammers_type.add_tool(hammer_1)
+    hammers_type.add_tool(hammer_2)
+    watering_type.add_tool(watering_1)
+    watering_type.add_tool(watering_2)
+
+    print(category._all_types)
 
     t = time.perf_counter()
     print(category.search_by_category('d'))
+    print(f'time used : {(time.perf_counter() - t)*(10*6):.5f} us')
+
+    print(category._all_tools)
+
+    t = time.perf_counter()
+    print(category.search_by_name('t'))
     print(f'time used : {(time.perf_counter() - t)*(10*6):.5f} us')
