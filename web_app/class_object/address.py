72a4e5faf4c58656163f31c9adf54d4549296992
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-03-30 11:37:54 +0700

    add the return object with the string

--- web_app/class_object/address.py
+++ web_app/class_object/address.py
@@ -15,14 +15,17 @@ class Address:
         self._postal_code = postal_code
 
     
-    def get_addresse(self,name,company,country,state,city,address,phone_number,postal_code):
+    def get_address(self,name,company,country,state,city,address,phone_number,postal_code):
         return Address(name,company,country,state,city,address,phone_number,postal_code)
 
-    def update_addresse(self):
+    def update_address(self):
         return print('Address has updated')
 
-    def edit_addresse(self, name, company, country, state, city , address, phone_number, postal_code):
+    def edit_address(self, name, company, country, state, city , address, phone_number, postal_code):
         if  Address(name,company,country,state,city,address,phone_number,postal_code)._name == self._name:
             return Address(name,company,country,state,city,address,phone_number,postal_code)
         
-    
+    def __repr__(self) -> str:
+        return "{}".format(self._name +','+"{}".format(self._company)+','+"{}".format(self._country)+','+"{}".format(self._state)+
+                           ','+"{}".format(self._city)+','+"{}".format(self._phone_number)+
+                           ','+"{}".format(self._address)+','+"{}".format(self._phone_number)+','+"{}".format(self._postal_code))
