50226387bf2f7794e8dc214d78958c6e5bcc6de5
ReallyWarm <warmkitjarak@gmail.com>
2023-03-31 00:41:43 +0700

    Change in System add to system cart method

--- web_app/class_object/system.py
+++ web_app/class_object/system.py
@@ -24,9 +24,8 @@ class System():
     def search_coupon(self): 
         return self._server_coupon
 
-    def add_to_cart(self, tool, amount):
-        item = Item(tool, amount)
-        self._system_cart.add_item(item)
+    def add_to_cart(self, tool, buy_amount):
+        self._system_cart.add_item(tool, buy_amount)
 
     def add_customerinfo(self, customer):
         self._customerinfo.append(customer)
