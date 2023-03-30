6baf4cc9b8757dbe6f75126c18b152c9e1924192
pithakpong <111265617+pithakpong@users.noreply.github.com>
2023-03-30 22:44:22 +0700

    edit manage wishlist and manage coupon

--- web_app/class_object/admin.py
+++ web_app/class_object/admin.py
@@ -5,15 +5,3 @@ class Admin():
         self._email = email
         self._username = username
         self._password = password 
-    def request_system(self,information,system,coupon_search = None):
-        if information == "search_coupon" : 
-            return system.search_coupon() 
-        if information == "find_coupon"  :
-            return system.find_coupon(coupon_search) 
-    def manage_coupons(self,event,system,code = None,discount_value = None,name = None,modify_coupon = None,coupon = None): 
-        if event == "create_coupon" : 
-            return system.create_coupon(code,discount_value,name)
-        if event == "modify_coupon" :  
-            return system.modify_coupon(modify_coupon,code,discount_value,name)
-        if event == "delete_coupon" : 
-            return system.delete_coupon(coupon)
