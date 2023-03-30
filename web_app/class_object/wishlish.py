66e9d741a62525525a4413f84d538b617a723783
ReallyWarm <warmkitjarak@gmail.com>
2023-03-31 00:35:56 +0700

    Add Wishlist method and fixing bugs

--- web_app/class_object/wishlish.py
+++ web_app/class_object/wishlish.py
@@ -1,30 +1,46 @@
+from tool import Tool, Item
+from shoppingcart import ShoppingCart
+
 class Wishlist:
     def __init__(self) -> None:
-        self._wish_product = [ ]
+        self._wish_product = []
         self._quantity = 0
         self._price = 0.00
 
-    def add_item(self,item):
-        if item not in self._wish_product:
-            self._wish_product.append(item)
-        else : 
-            for i in self._wish_product: 
-                if i._name == item._name : 
-                    i = item 
-                break
+    @property
+    def wish_product(self):
+        return self._wish_product
+    
+    def find_item(self, tool:Tool):
+        for wish_item in self.wish_product:
+            if tool is wish_item.tool:
+                return wish_item
+        return None
 
-    def set_item(self):
-        pass
+    def add_item(self, tool:Tool, buy_amount:int):
+        wish_item = self.find_item(tool)
+        if wish_item is not None:
+            wish_item.set_buy_amount(buy_amount)
+        else:
+            self._wish_product.append(Item(tool, buy_amount))
 
-    def get_items(self,item_search):
-        for item in self._wish_product:
-            if item._name == item_search._name: 
-                return item 
-            break
+    def set_item_amount(self, tool:Tool, buy_amount:int):
+        item = self.get_items(tool)
+        item.set_buy_amount(buy_amount)
 
-    def delete_item(self,item):
+    def get_items(self, tool:Tool):
+        for wish_item in self.wish_product:
+            if tool == wish_item.tool:
+                return wish_item 
+
+    def delete_item(self, tool:Tool):
+        item = self.get_items(tool)
         self._wish_product.remove(item)
 
-    def send_to_cart(self):
-        pass
+    def send_to_cart(self, cart:ShoppingCart):
+        for wish_item in self.wish_product:
+            cart.add_by_item(wish_item)
+        self._wish_product.clear()
 
+    def calculate_price(self):
+        pass
