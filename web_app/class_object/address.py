77eae3f9d664262dbf71008983ed05f4ae88f193
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-03-31 23:30:56 +0700

    modify the edit_address method

--- web_app/class_object/address.py
+++ web_app/class_object/address.py
@@ -23,7 +23,14 @@ class Address:
 
     def edit_address(self, name, company, country, state, city , address, phone_number, postal_code):
         if  Address(name,company,country,state,city,address,phone_number,postal_code)._name == self._name:
-            return Address(name,company,country,state,city,address,phone_number,postal_code)
+            self._name = name
+            self._company = company
+            self._country = country
+            self._state = state
+            self._city = city
+            self._address = address
+            self._phone_number = phone_number
+            self._postal_code = postal_code
         
     def __repr__(self) -> str:
         return "{}".format(self._name +','+"{}".format(self._company)+','+"{}".format(self._country)+','+"{}".format(self._state)+
