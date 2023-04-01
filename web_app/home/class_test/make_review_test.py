a857083fd18e8880f1012cf4519995f1e209ffe4
BBamkorphai <111273264+BBamkorphai@users.noreply.github.com>
2023-04-01 14:44:49 +0700

    Sequence: make review (better test)

--- web_app/home/class_test/make_review_test.py
+++ web_app/home/class_test/make_review_test.py
@@ -6,5 +6,15 @@ from tool import Tool
 
 test_drill = Tool('01x', 'Testing drill', 'POWER', 'idk', 10, None, 1000.00, 'Drills')
 korphai = CustomerInfo("Prakrittipon", "Sommool", "korphaisk@gmail.com", "KMITL")
-korphai.create_review(test_drill)
+meawhod = CustomerInfo("prakonthakrit", "Picthaya", "meawnoy@gmail.com", "KMITL")
+korphai.create_review(test_drill, "test_review1", "good_tool", 4.5, "4/1/2023")
+meawhod.create_review(test_drill, "test_review2", "that broke", 1.5, "4/1/2023")
+
 print("------------------------------------")
+print(korphai.my_reviewed[0])
+print("------------------------------------")
+print(test_drill.review_list[0])
+print("------------------------------------")
+print(test_drill.review_list[1])
+print("------------------------------------")
+print(test_drill.rating)
