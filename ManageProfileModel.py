43aeddfcb633a44dc6bfa3beaeeffc738c21b9a4
Kfirm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-04-18 19:47:24 +0700

    fix:fix paramitor in function

--- ManageProfileModel.py
+++ ManageProfileModel.py
@@ -32,29 +32,27 @@ customer[2].create_review(test_drill, "test_review1", "good_tool", 4.5, "4/1/202
 
 #create a new customer
 @app.post('/customer/{first_name}/')
-async def create_customer(first_name:str, last_name:str, email:str, company_name:str) ->dict:
-    new_customer = CustomerInfo(first_name, last_name, email, company_name)
+async def create_customer(customer:dict)->dict:
+    new_customer = CustomerInfo(customer["first_name"], customer["last_name"], customer["email"],customer["company_name"])
     Sys.add_customerinfo(new_customer)
-    customer.append(new_customer)
     return {'data':new_customer}
 
 #create a new address
 @app.post('/customer/address/{name}/')
-async def create_address(name:str, company:str, country:str, state:str,
-                 city:str, address:str, phone_number:str, postal_code:str)->dict:
+async def create_address(newaddress:dict)->dict:
     for customer in Sys.customerinfo:
-        if customer.first_name == name:
-            customer.create_address(name,company, country, state, city, address, phone_number,postal_code)
+        if customer.first_name == newaddress["name"]:
+            customer.create_address(newaddress["name"],newaddress["company"],newaddress["country"],newaddress[ "state"], newaddress["city"], newaddress["address"], newaddress["phone_number"],newaddress["postal_code"])
             return {"data":"You have create the address successfully."}
             
     return{"data":"Unable to create the address."}
 
-#delete the address     
+# #delete the address     
 @app.delete('/cusrtomer/address/{name}',tags=['address'])     
-async def delete_address(name:str) ->dict:
+async def delete_address(data:dict) ->dict:
     for customer in Sys.customerinfo:
-        if customer.first_name == name:
-            customer.delete_address(name)
+        if customer.first_name == data["name"]:
+            customer.delete_address(data["name"])
             return {"data":"You have delete the address successfully"}
     return {"data":"Unable to delete the address"}    
 
@@ -67,21 +65,20 @@ async def get_address(name:str)->dict:
           return {"data":f'Your address  {address}'}
   return {"data":"Sorry cannot found your address in system. Please try again!"}    
       
-# Have 404 error responses  
-#edit the address       
-@app.put('/customer/address/{address}',tags=['address'])
-async def edit_address(name:str=None, company:str=None, country:str=None, state:str=None, city:str=None ,address:str=None, phone_number:str=None, postal_code:str=None) ->dict:
+# edit the address       
+@app.put('/customer/address/')
+async def edit_address(address:dict) ->dict:
     for customer in Sys.customerinfo:
-        if customer.first_name == name:
-            new_address = customer.get_address(name).edit_address(company, country,state,city,address,phone_number, postal_code)
-        return {"data":f"Your edit address is successfully {new_address}"}
+        if customer.first_name == address["name"]:
+            customer.get_address(address["name"]).edit_address(company=address["company"],country=address["country"],state=address[ "state"], city=address["city"], address=address["address"], phone_number=address["phone_number"],postal_code=address["postal_code"])
+            return {"data":f"Your edit address is successfully {customer.address}"}
     return {"data":"Unable to edit address. Please try again"}
 
 @app.get('/customer/order{name}')
 async def check_order(name:str)->dict:
     for customer in Sys.customerinfo:
         if customer.first_name == name:
-            order = customer.my_order[0]
+            order = customer.my_order
             return {"data":f"Your order is {order}"}
     return {"data":"Not found this order. Please try again"}    
 
@@ -89,6 +86,5 @@ async def check_order(name:str)->dict:
 async def check_review(name:str)->dict:
     for customer in Sys.customerinfo:
         if customer.first_name == name:
-            review = customer.my_reviewed[0]
-            return {"data":f"Your review is {review}"}
+            return {"data":f"Your review is {customer.my_reviewed}"}
     return {"data":"Not found this review. Please try again"}
