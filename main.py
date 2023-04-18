839343e35391e209cebe1c8304e8be4468694dc1
ReallyWarm <warmkitjarak@gmail.com>
2023-04-19 03:11:59 +0700

    add and test login

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
 sys.path.append('./class_object/')
-from fastapi import FastAPI
+import secrets
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBasicCredentials, HTTPBearer, HTTPBasic
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
 from system import System
 from category import TypeOfTool, SubtypeOfTool
 from tool import Tool
@@ -9,10 +14,19 @@ from app_database import add_database_users, add_database_system, add_database_u
 
 app = FastAPI()
 system = System()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login",scheme_name="JWT")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "608672d916b11ac31e9ac553d5418caec4052bca348f2d06d4440aaacce155b0"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 add_database_users(system)
 add_database_system(system)
 add_database_userdata(system)
+
+def object_to_dict(object):
+    return object.__dict__
 
 # CATEGORY
 @app.post("/system/category/typeoftools/")
@@ -176,3 +190,98 @@ async def check_review(name:str)->dict:
             return {"data":f"Your review is {customer.my_reviewed}"}
     return {"data":"Not found this review. Please try again"}
 
+# LOGIN
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(username: str):
+    return system.search_user(username)
+    
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# def get_current_user(credentials: HTTPBasicCredentials = Depends(HTTPBearer())):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+
+#     token = credentials.credentials
+
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         if payload is None:
+#             raise credentials_exception
+#         return credentials
+#     except JWTError:
+#         raise credentials_exception
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_username = username
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_username)
+    if user is None:
+        raise credentials_exception
+    return object_to_dict(user)
+
+@app.post('/signup', summary="Create new user", response_model=dict)
+async def create_user(signup_data:dict):
+    # {"username":"","password":"","first_name":"","last_name":"","email":"","company_name":""}
+    user = system.search_user(signup_data['username'])
+    if user is not None:
+            raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this username already exist")
+    new_customer = CustomerInfo(signup_data["username"], get_password_hash(signup_data["password"]), 
+                                signup_data["first_name"], signup_data["last_name"], 
+                                signup_data["email"], signup_data["company_name"])
+    system.add_customerinfo(new_customer)
+    return {'data':new_customer}
+
+@app.post("/login", summary="Create access tokens for login user", response_model=dict)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"})
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/me", response_model=dict)
+async def read_users_me(current_user: dict = Depends(get_current_user)):
+    return current_user
