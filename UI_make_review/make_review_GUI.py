1f7ae261c29379395b1037b53a699f8b598d9585
BBamkorphai <111273264+BBamkorphai@users.noreply.github.com>
2023-04-19 13:41:08 +0700

    update_UI_make_review

--- UI_make_review/make_review_GUI.py
+++ UI_make_review/make_review_GUI.py
@@ -13,26 +13,40 @@ class make_review_GUI:
         self.root.title("Make Review")
         self.root.geometry("500x500")
         self.root.resizable(False, False)
-        self.root.configure(background="white")
+        self.root.configure(background="#f7f7f7")  # Set background color
         self.root.config(cursor="hand2")
-        Label(text="write review").pack(anchor='center')
-        self.ent1 = Entry(self.root)
-        self.ent1.place(x = 50, y= 100)
-        self.ent2 = Text(width= 20, height= 10)
-        self.ent2.place(x = 50, y=200)
+        
+        # Create a label for the heading
+        Label(text="Write Review", fg="#333", font=("Helvetica", 18, "bold")).pack(pady=20)
+        
+        # Create a label for the review title
+        Label(text="Title:", font=("Helvetica", 12)).place(x=50, y=60)
+        self.ent1 = Entry(self.root, font=("Helvetica", 12), bd=1, relief=SOLID)  # Set border properties
+        self.ent1.place(x=50, y=90, width=400)
+        
+        # Create a label for the review comment
+        Label(text="Comment:", font=("Helvetica", 12)).place(x=50, y=130)
+        self.ent2 = Text(width=40, height=10, font=("Helvetica", 12), bd=1, relief=SOLID)  # Set border properties
+        self.ent2.place(x=50, y=160)
+        
+        # Create a label for the review rating
+        Label(text="Rating:", font=("Helvetica", 12)).place(x=50, y=350)
         rating = StringVar()
-        self.ent3 = ttk.Combobox(self.root, width=30, textvariable=rating)
+        self.ent3 = ttk.Combobox(self.root, width=30, textvariable=rating, font=("Helvetica", 12))
         self.ent3['value'] = (0,1,2,3,4,5)
-        self.ent3.place(x = 50, y=400)
+        self.ent3.place(x=50, y=380)
         self.ent3.current()
-        Button(text="sent review", command=self.send_review).place(x=300, y=400)
+        
+        # Create a button to send the review
+        Button(text="Submit Review", font=("Helvetica", 12, "bold"), bg="#333", fg="#fff", command=self.send_review).place(x=180, y=430)
+        
         self.root.mainloop()
 
     def send_review(self):
         data = {
-	            "User":self.user,
-	            "tool":self.tool,
-	            "head_review":self.ent1.get(),
+                "User":self.user,
+                "tool":self.tool,
+                "head_review":self.ent1.get(),
                 "comment":self.ent2.get("1.0","end-1c"),
                 "rating":self.ent3.get(),
                 "date_of_review":datetime.now().strftime("%d-%m-%Y")
@@ -43,7 +57,7 @@ class make_review_GUI:
         r = requests.post("http://127.0.0.1:8000/tool/make_review",json = data)
         respon = json.loads(r.text)
         if respon == {'data': 'A new review is added!'}:
-            messagebox.showinfo(title="review respon",message="your review is added!")
+            messagebox.showinfo(title="Review Response", message="Your review has been added!")
         print(respon)
 
 make_review_GUI('NorNor','Testing drill')
