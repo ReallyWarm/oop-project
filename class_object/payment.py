00b7d3d19bf37cd999d1b6e9f06ba68489412cc4
ReallyWarm <warmkitjarak@gmail.com>
2023-04-23 21:14:53 +0700

    Update Payment class property

--- class_object/payment.py
+++ class_object/payment.py
@@ -1,22 +1,28 @@
 import datetime
 import random
 import string
+
 class Payment: 
-    def __init__(self, total_amount:float, card:str, discount_amount:int = 0) -> None:
-        self.__total_amount = total_amount
+    def __init__(self, total_price:float, shipping_price:float, card:str, discount_price:int = 0) -> None:
+        self.__total_price = total_price
+        self.__shipping_price = shipping_price
         self.__date_create = datetime.datetime.now()
         self.__id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
         self.__card = card 
-        self.__discount_amount = discount_amount * self.__total_amount/100
-        self.__final_price = self.__total_amount - self.__discount_amount
+        self.__discount_price = discount_price * self.__total_price/100
+        self.__final_price = self.__total_price + shipping_price - self.__discount_price
 
     @property
-    def total_amount(self):
-        return self.__total_amount
+    def total_price(self):
+        return self.__total_price
     
     @property
-    def discount_amount(self):
-        return self.__discount_amount
+    def shipping_price(self):
+        return self.__shipping_price
+    
+    @property
+    def discount_price(self):
+        return self.__discount_price
     
     @property
     def final_price(self):
