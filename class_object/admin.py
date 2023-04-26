c1cfe96f5f44126bec8de6ccbfc035e7d6403bf4
ReallyWarm <warmkitjarak@gmail.com>
2023-04-26 19:07:11 +0700

    Make Admin a subclass of User

--- class_object/admin.py
+++ class_object/admin.py
@@ -1,7 +1,5 @@
-class Admin():
-    def __init__(self, username:str, password:str, first_name:str, last_name:str, email:str) -> None:
-        self._first_name = first_name
-        self._last_name = last_name
-        self._email = email
-        self._username = username
-        self._password = password 
+from user import User
+
+class Admin(User):
+    def __init__(self, username:str, hashed_password:str, first_name:str, last_name:str, email:str) -> None:
+        User.__init__(self, username, hashed_password, first_name, last_name, email)
