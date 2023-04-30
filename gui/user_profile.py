d73fa04f9b9076d8055919f17e1dc5ab7f84160b
Pornthep Thammawong <106590971+Kfirm1208@users.noreply.github.com>
2023-04-30 17:53:37 +0700

    use pack substitute by gride

--- gui/user_profile.py
+++ gui/user_profile.py
@@ -13,16 +13,23 @@ class Profile(tk.Frame):
         self.address = self.user_address()
         self.order = self.user_order()
         self.review = self.user_review()
+        self.firstname_title = tk.Label(self,text="First Name", font= ('Arail',12,'bold'))
+        self.lastname_title = tk.Label(self,text="Last Name", font=('Arail',12,'bold'))
+        self.email_title = tk.Label(self,text="E-mail", font=('Arail',12,'bold'))
+        self.company_title = tk.Label(self,text="Company", font=('Arail',12,'bold'))
+        self.address_title = tk.Label(self,text="Address", font=('Arail',12,'bold'))
+        self.order_title = tk.Label(self,text="Order", font=('Arail',12,'bold'))
+        self.review_title = tk.Label(self,text="Review", font=('Arail',12,'bold'))
         self.username_label = tk.Label(self, text=self.username, font=("Arial", 18, "bold"))
         self.firstname_label = tk.Label (self,text=self.firstname,font=("Arial", 12))
         self.lastname_label = tk.Label(self,text=self.lastname,font=('Arail',12))
         self.email_label = tk.Label(self,text=self.email,font=('Arail',12))
         self.company_label = tk.Label(self,text=self.company,font=('Arail',12))
-        self.address_text = tk.Text(self,height=10,width=60)
+        self.address_text = tk.Text(self,height=5,width=40)
         self.order_text = tk.Text(self,height=5,width=40)
         self.review_text = tk.Text(self,height=5,width=40)
         
-        self.create_widget()
+        # self.create_widget()
         
     def user_name_data(self): 
         user =requests.get("http://127.0.0.1:8000/me").json()
@@ -98,16 +105,24 @@ class Profile(tk.Frame):
                 return username['first_name']['_my_review']
         return {"data":"guest"}
      
-    
+    # idea delete all pages and paste a new item
     def delete_data(self):
+        
         self.username_label.destroy()
         self.firstname_label.destroy()
+        self.firstname_title.destroy()
         self.lastname_label.destroy()
+        self.lastname_title.destroy()
         self.email_label.destroy()
+        self.email_title.destroy()
         self.company_label.destroy()
+        self.company_title.destroy()
         self.address_text.destroy()
+        self.address_title.destroy()
         self.order_text.destroy()
+        self.order_title.destroy()
         self.review_text.destroy()
+        self.review_title.destroy()
         
      
     def update_data(self):
@@ -124,112 +139,126 @@ class Profile(tk.Frame):
     def update_username(self):
         self.username = self.user_name_data()
         self.username_label = tk.Label(self, text=self.username, font=("Arial", 18, "bold"))
-        self.username_label.grid(row=0,column=1,columnspan=2,pady=30)
+        self.username_label.pack()
         print(self.username)
         
     def update_firstname(self):
         self.firstname = self.user_firstname()
+        self.firstname_title = tk.Label(self,text="First Name", font= ('Arail',12,'bold'))
+        self.firstname_title.pack()
         self.firstname_label = tk.Label (self,text=self.firstname,font=("Arial", 12))
-        self.firstname_label.grid(row=1, column=1, padx=10, pady=5)
+        self.firstname_label.pack()
         print(self.firstname)
     
     
     def update_lastname(self):
         self.lastname = self.user_lastname()
+        self.lastname_title = tk.Label(self,text="Last Name", font=('Arail',12,'bold'))
+        self.lastname_title.pack()
         self.lastname_label = tk.Label(self,text=self.lastname,font=('Arail',12))
-        self.lastname_label.grid(row=2, column=1, padx=10, pady=5)
+        self.lastname_label.pack()
         print(self.lastname)
     
     def update_email(self):
         self.email = self.user_email()
+        self.email_title = tk.Label(self,text="E-mail", font=('Arail',12,'bold'))
+        self.email_title.pack()
         self.email_label = tk.Label(self,text=self.email,font=('Arail',12))
-        self.email_label.grid(row=3, column=1, padx=10, pady=5)
+        self.email_label.pack()
         print(self.email)
         
     def update_company(self):
         self.company =self.user_company()
+        self.company_title = tk.Label(self,text="Company", font=('Arail',12,'bold'))
+        self.company_title.pack()
         self.company_label = tk.Label(self,text=self.company,font=('Arail',12))
-        self.company_label.grid(row=4, column=1, padx=10, pady=5)
+        self.company_label.pack()
         print(self.company)
     
     def update_address(self):
         self.address = self.user_address()
-        self.address_text = tk.Text(self,height=10,width=80)
+        self.address_title = tk.Label(self,text="Address", font=('Arail',12,'bold'))
+        self.address_title.pack()
+        self.address_text = tk.Text(self,height=5,width=40)
         self.address_text.insert('end',self.address)
-        self.address_text.grid(row=5, column=0, padx=10, pady=5, sticky=tk.E)
+        self.address_text.pack()
         print(self.address)
         
     def update_order(self):
         self.order = self.user_order()
+        self.order_title = tk.Label(self,text="Order", font=('Arail',12,'bold'))
+        self.order_title.pack()
         self.order_text = tk.Text(self,height=5,width=40)
         self.order_text.insert('end',self.order)
-        self.order_text.grid(row=6, column=1, padx=10, pady=5)
+        self.order_text.pack()
         print(self.order)
 
     def update_review(self):
         self.review = self.user_review() 
+        self.review_title = tk.Label(self,text="Review", font=('Arail',12,'bold'))
+        self.review_title.pack()
         self.review_text = tk.Text(self,height=5,width=40)
         self.review_text.insert('end',self.review)  
-        self.review_text.grid(row=7, column=1, padx=10, pady=5)
+        self.review_text.pack()
         print(self.review)
         
     def create_widget(self):
         
         #create username label
-        self.username_label.grid(row=0,column=1,columnspan=2,pady=30)
+        self.username_label.pack()
       
         #create title firstname
-        self.firstname_title = tk.Label(self,text="First Name:", font= ('Arail',12,'bold'))
-        self.firstname_title.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.firstname_title = tk.Label(self,text="First Name:", font= ('Arail',12,'bold'))
+        self.firstname_title.pack()
         
         #show firstname data
-        self.firstname_label.grid(row=1, column=1, padx=10, pady=5)
+        self.firstname_label.pack()
       
         #create title lastname
-        self.lastname_title = tk.Label(self,text="Last Name:", font=('Arail',12,'bold'))
-        self.lastname_title.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.lastname_title = tk.Label(self,text="Last Name:", font=('Arail',12,'bold'))
+        self.lastname_title.pack()
        
-       #show lastname data
-        self.lastname_label.grid(row=2, column=1, padx=10, pady=5)
+    #    #show lastname data
+        self.lastname_label.pack()
         
-        #create title email
-        self.email_title = tk.Label(self,text="E-mail:", font=('Arail',12,'bold'))
-        self.email_title.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
+    #     #create title email
+    #     # self.email_title = tk.Label(self,text="E-mail:", font=('Arail',12,'bold'))
+        self.email_title.pack()
       
-        #show email data
-        self.email_label.grid(row=3, column=1, padx=10, pady=5)
+    #     #show email data
+        self.email_label.pack()
        
         #create title company
-        self.company_title = tk.Label(self,text="Company:", font=('Arail',12,'bold'))
-        self.company_title.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.company_title = tk.Label(self,text="Company:", font=('Arail',12,'bold'))
+        self.company_title.pack()
       
         #show company data
-        self.company_label.grid(row=4, column=1, padx=10, pady=5)
+        self.company_label.pack()
         
        
         #create title address
-        self.address_title = tk.Label(self,text="Address:", font=('Arail',12,'bold'))
-        self.address_title.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.address_title = tk.Label(self,text="Address:", font=('Arail',12,'bold'))
+        self.address_title.pack()
         
         #show address
         self.address_text.insert('end',self.address)
-        self.address_text.grid(row=5, column=1, padx=10, pady=5,sticky=tk.E)
+        self.address_text.pack()
        
         #create title order
-        self.order_title = tk.Label(self,text="Order:", font=('Arail',12,'bold'))
-        self.order_title.grid(row=6, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.order_title = tk.Label(self,text="Order:", font=('Arail',12,'bold'))
+        self.order_title.pack()
        
         #show order data
         self.order_text.insert('end',self.order)
-        self.order_text.grid(row=6, column=1, padx=10, pady=5)
+        self.order_text.pack()
        
         #create title review
-        self.review_title = tk.Label(self,text="Review:", font=('Arail',12,'bold'))
-        self.review_title.grid(row=7, column=0, padx=10, pady=5, sticky=tk.W)
+        # self.review_title = tk.Label(self,text="Review:", font=('Arail',12,'bold'))
+        self.review_title.pack()
         
         #show review data
         self.review_text.insert('end',self.review)
-        self.review_text.grid(row=7, column=1, padx=10, pady=5)
+        self.review_text.pack()
       
         
  
