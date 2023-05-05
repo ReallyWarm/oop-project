c833dd00cc66ce6d2bbabd42a39e3998217da393
Pornthep Thammawong <106590971+Kfirm1208@users.noreply.github.com>
2023-05-05 15:41:48 +0700

    Fix the bug

--- gui/make_review.py
+++ gui/make_review.py
@@ -33,24 +33,29 @@ class MakeReview(tk.Frame):
         tk.Button(self,text="Submit Review", font=("Helvetica", 12, "bold"), bg="#333", fg="#fff", command=self.send_review).place(x=180, y=500)
 
     def send_review(self):
-        if self.ent3.get() != "0" or "1" or "2" or "3" or "4" or "5":
-            messagebox.showinfo(title="Review Response", message="Your must input int 0 - 5")
-            pass
-        elif int(self.ent3.get()) > 5 or int(self.ent3.get()) < 0:
-            messagebox.showinfo(title="Review Response", message="Your must input int 0 - 5")
-        else:
-            data = {
-                    "User":self.master.first_name,
-                    "tool":self.tool,
-                    "head_review":self.ent1.get(),
-                    "comment":self.ent2.get("1.0","end-1c"),
-                    "rating":self.ent3.get(),
-                    "date_of_review":datetime.now().strftime("%d-%m-%Y")
-                }
-            
-            r = requests.post("http://127.0.0.1:8000/tool/make_review",json = data)
-            respon = json.loads(r.text)
-            if respon == {'data': 'A new review is added!'}:
-                messagebox.showinfo(title="Review Response", message="Your review has been added!")
+        try :
+            if int(self.ent3.get()) > 5 or int(self.ent3.get()) < 0:
+                messagebox.showinfo(title="Review Response", message="Your must input int 0 - 5")
+                return
             else:
-                messagebox.showinfo(title="Review Response", message="Yor must login first")
+                data = {
+                        "User":self.master.first_name,
+                        "tool":self.tool,
+                        "head_review":self.ent1.get(),
+                        "comment":self.ent2.get("1.0","end-1c"),
+                        "rating":self.ent3.get(),
+                        "date_of_review":datetime.now().strftime("%d-%m-%Y")
+                    }
+                
+                r = requests.post("http://127.0.0.1:8000/tool/make_review",json = data)
+                respon = json.loads(r.text)
+                if respon == {'data': 'A new review is added!'}:
+                    messagebox.showinfo(title="Review Response", message="Your review has been added!")
+                else:
+                    messagebox.showinfo(title="Review Response", message="Yor must login first")
+                    
+        except :  
+            messagebox.showinfo(title="Review Response", message="Your must input int 0 - 5")    
+                  
+        print(self.ent3.get())
+        print(type(self.ent3.get()))
